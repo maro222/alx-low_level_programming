f13a1487b1f2d7c0799e1b08587a43e6f2436fd4
omar <omar@alx.com>
2023-03-23 08:07:41 -0700

    task 7 ,8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: A C program prints by write in library unistd.h
+ *
+ * Return: Always 1 (unsuccessful)
+ */
+
+int main(void)
+{
+	write(2,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
