57089d43cf395f6c2aa08a7a638edd1c6c1671da
omar <omar@alx.com>
2023-03-24 18:23:06 -0700

    committing task 6 and 7

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_line - print straight line
+ *
+ * @n: input
+ *
+ * Return: no return
+*/
+
+void print_line(int n)
+{
+	if (n == 0)
+		_putchar('\n');
+	else
+	{
+		int j;
+
+		for (j = 0; j < n; j++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
