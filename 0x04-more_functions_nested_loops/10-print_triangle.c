117bf8ed05c89c85084c602fef11a7b65d62b556
omar <omar@alx.com>
2023-03-24 19:35:22 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -16,9 +16,9 @@ void print_triangle(int size)
 		_putchar('\n');
 	else
 	{
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
-			for (j = 0; j < size; j++)
+			for (j = 1; j <= size; j++)
 			{
 				if ((i + j) <= size)
 					_putchar(' ');
