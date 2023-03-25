13a989dd51958302d731d41ee1c97ecc9976ce3b
omar <omar@alx.com>
2023-03-24 18:30:36 -0700

    unknown fix task 6 and 7

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,12 +10,12 @@
 
 void print_line(int n)
 {
+	int j;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		int j;
-
 		for (j = 0; j < n; j++)
 		{
 			_putchar('_');
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -10,20 +10,18 @@
 
 void print_diagonal(int n)
 {
+	int i , k;
+
 	if (n <= 0)
 		_putchar('\n');
 	else
 	{
-		int i;
-
 		for (i = 0; i < n; i++)
 		{
-			int k;
-
 			for (k = 0; k < i; k++)
 			{
 				if (k == i - 1)
-					_putchar('\\');
+					_putchar('/');
 				_putchar(' ');
 			}
 		}
