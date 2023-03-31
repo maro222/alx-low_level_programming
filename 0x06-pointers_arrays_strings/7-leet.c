e6d2bfbfe7c5fccf4a886437e3abb5316d8f4ef8
omar <omar@alx.com>
2023-03-30 19:37:06 -0700

    task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ char *leet(char *c)
 	char key[] = {'A', 'E', 'O', 'T', 'L'};
 	char value[] = {4, 3, 0, 7, 1};
 	int i;
-	char *ptr =c;
+	char *ptr = c;
 
 	while (*c)
 	{
@@ -22,5 +22,6 @@ char *leet(char *c)
 			if (*c == key[i] || *c == key[i] + 32)
 				*c = 48 + value[i];
 		}
+		c++;
 	}
 	return (ptr);
