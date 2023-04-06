95de08733e0f29305112eddf7ed414fd536672bb
omar <omar@alx.com>
2023-04-05 18:21:29 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,18 +3,26 @@
 /**
  * _sqrt_recursion - prints a string , followed by a new line
  * @n: input
- * Return: void
+ * Return: int
  */
 int _sqrt_recursion(int n)
 {
-	square(n , 1);
+	return (square(n, 1));
 }
+
+/**
+ * square - auxilary function
+ * @n: input
+ * @i: input
+ * Return: int
+ */
+
 int square(int n, int i)
 {
 	if (i * i == n)
-		return (n);
+		return (i);
 	else if (i * i < n)
-		_sqrt_recursion(n , i + 1);
+		square(n, i + 1);
 	else
 		return (-1);
 }
