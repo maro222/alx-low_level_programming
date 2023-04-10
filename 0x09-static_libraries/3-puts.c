3d2f0cb8b41b2d4d1cffb3ee8aa9331439f89c31
omar <omar@alx.com>
2023-04-10 06:14:41 -0700

    task 0

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts - swap between 2 pointers
+ *
+ * @str: string parameter input
+ *
+ * Return: nothing
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i) != '\0'; i++)
+		_putchar(*(str + i));
+	_putchar('\n');
+}
