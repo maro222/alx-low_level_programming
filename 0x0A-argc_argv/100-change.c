ec65a5668c9c2964dc3982a3162e8acf5ad3231e
omar <omar@alx.com>
2023-04-10 19:59:19 -0700

    task 100

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiply 2 arguments
+ * @argc: input
+ * @argv: input
+ * Return: 0 success , 1 fail
+ *
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, coins = 0, money = atoi(argv[1]);
+		int arr[] = {25, 10, 5, 2, 1};
+
+		for (i = 0, i < 5; i++)
+		{
+			if (money >= arr[i])
+			{
+				coins += money / arr[i];
+				money %= arr[i];
+				if (money % arr[i] == 0)
+					break;
+			}
+		}
+	printf("%d\n", coins);
+	}
+	else
+		return (printf("Error\n"), 1);
+	return (0);
+}
