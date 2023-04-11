cf3f0737842b09b776eef0be2fadd8ae2ec11817
omar <omar@alx.com>
2023-04-10 18:21:19 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,5 +1,47 @@
 #include <stdio.h>
-#include <stdlib>
+
+/**
+ * atoi - converts
+ * @s: string input
+ * Return: 0 success , 1 fail
+ *
+ */
+
+int atoi(char *s)
+{
+	int i, d, n, len, f, digit;
+
+	i = 0;
+	d = 0;
+	n = 0;
+	len = 0;
+	f = 0;
+	digit = 0;
+
+	while (s[len] != '\0')
+		len++;
+	while (i < len && f == 0)
+	{
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
+			if (d % 2)
+				digit = -digit;
+			n = n * 10 + digit;
+			f = 1;
+			if (s[i + 1] < '0' || s[i + 1] > '9')
+				break;
+			f = 0;
+		}
+		i++
+	}
+	if (f == 0)
+		return (0);
+	return (n);
+}
+
 
 /**
  * main - multiply 2 arguments
@@ -8,7 +50,6 @@
  * Return: 0 success , 1 fail
  *
  */
-
 int main(int argc, char *argv[])
 {
 	int mul;
