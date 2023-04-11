a45688cb631d4095dd097ea0c52fdb3a9b250432
omar <omar@alx.com>
2023-04-10 19:07:34 -0700

    task 3,4

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,7 +12,7 @@ int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
 
-	if (argc < 3 || argc > 3) 
+	if (argc < 3 || argc > 3 
 	{
 		printf("Error\n");
 		return (1);
--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,12 +11,12 @@
 
 int main(int argc, char *argv[])
 {
-	int i, add = 0;
+	int add = 0;
 	char *c;
 
-	while(--argc)
+	while (--argc)
 	{
-		for (c =argv[argc]; *c; c++)
+		for (c = argv[argc]; *c; c++)
 			if (*c < '0' || *c > '9')
 				return (printf("Error\n"), 1);
 		add += atoi(argv[argc]);
