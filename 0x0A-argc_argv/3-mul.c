e5df1003433cda209c9d4754d7593a8e17e3d657
omar <omar@alx.com>
2023-04-10 18:44:23 -0700

    task 3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -54,17 +54,14 @@ int main(int argc, char *argv[])
 {
 	int mul, num1, num2;
 
-	if !(argc < 3 || argc > 3) 
-	{
-		num1 = atoi(argv[1]);
-		num2 = atoi(argv[2]);
-		mul = num1 * num2;
-		printf("%d\n", mul);
-	}
-	else
+	if (argc < 3 || argc > 3) 
 	{
 		printf("Error\n");
 		return (1);
 	}
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	mul = num1 * num2;
+	printf("%d\n", mul);
 	return (0);
 }
