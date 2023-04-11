fb214c14366496afea4cfeb2c529eba077286542
omar <omar@alx.com>
2023-04-10 17:24:25 -0700

    task 3,4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib>
 
 /**
  * main - multiply 2 arguments
@@ -16,7 +17,7 @@ int main(int argc, char *argv[])
 	{
 		if (argv[i] >= 48 || argv[i] <= 57)
 		{
-			add = add + (int)argv[i];
+			add = add + atoi(argv[i]);
 		}
 		else
 		{
