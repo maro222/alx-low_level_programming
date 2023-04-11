61ae57e6dad11a9da04aca8659412cba0e39485a
omar <omar@alx.com>
2023-04-10 18:32:13 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		if (argv[i] >= 48 && argv[i] <= 57)
+		if (argv[i] >= '48' && argv[i] <= '57')
 		{
 			add = add + atoi(argv[i]);
 		}
