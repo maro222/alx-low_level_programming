662abe0536261aed6460be9ad54d793b2110a366
omar <omar@alx.com>
2023-04-11 19:03:44 -0700

    task 3

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * alloc_grid - allocate an array of specific size
+ * @width: string
+ * @height: string
+ * Return: pointer success , Null fails
+ */
+
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+	int *arr =  malloc(height * sizeof(int));
+
+	if (width <= 0 || height <= 0 || arr == NULL)
+		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		arr[i] = malloc(width * sizeof(int));
+		if (arr[i] == NULL)
+		{
+			while (i--)
+				free(arr[i]);
+			free(arr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			arr[i][j] = 0;
+	}
+	return (arr);
+}
+
