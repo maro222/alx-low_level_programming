2de5647de06e26052517a4b7eeb1d8a8b9cb9293
omar <omar@alx.com>
2023-04-11 17:06:19 -0700

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -8,13 +8,14 @@
 
 char *_strdup(char *str)
 {
-	char *ch = malloc(sizeof(str));
+	int size;
+	char *ch = malloc(sizeof(*str));
 	unsigned int i = 0;
 
-	if (ch == NULL || sizeof(str) == 0)
+	if (ch == NULL || sizeof(*str) == 0)
 		return (NULL);
 
-	for (; i < sizeof(str); i++)
+	for (; i < sizeof(*str); i++)
 		ch[i] = str[i];
 	return (ch);
 }
