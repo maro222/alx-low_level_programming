527f08b4943bc06de17ce133e4f5b596e9623d6a
omar <omar@alx.com>
2023-04-18 18:34:03 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,7 +2,6 @@
 
 /**
  * new_dog - cpy a struct dog variable
- * @d: struct name
  * @name: dog name
  * @age: dog age
  * @owner: dog owner
@@ -12,4 +11,41 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	return (name, age, owner);
+	dog_t *dog;
+	int i, j;
+
+	if (name == NULL || age < 0 || owner == NULL)
+		return (NULL);
+	dog = (dog_t *)malloc(sizeof(dog_t));
+
+	if (!dog)
+		return (NULL);
+	for (i = 0; name[i] != '\0'; i++)
+	;
+	dog->name = malloc(sizeof(char) * (i + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	for (j = 0; owner[j] != '\0'; j++)
+	;
+	dog->owner = malloc(sizeof(char) * (j + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	i = 0;
+	j = 0;
+
+	for (; name[i] != '\0'; i++)
+		dog->name[i] = name[i];
+	for (; owner[j] != '\0'; j++)
+		dog->owner[i] = owner[i];
+	dog->name[i] = '\0';
+	dog->owner[i] = '\0';
+	dog->age = age;
+	return (dog);
+}
