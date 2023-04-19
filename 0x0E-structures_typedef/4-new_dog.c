3404e436871e6e478c17c7dc352ad9fc5cfc3e59
omar <omar@alx.com>
2023-04-18 19:04:17 -0700

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,7 +16,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	if (name == NULL || age < 0 || owner == NULL)
 		return (NULL);
-	dog = (dog_t *) malloc(sizeof(dog_t));
+	dog =  malloc(sizeof(dog_t));
 
 	if (dog == NULL)
 		return (NULL);
