d12bec9f56ec3fa216f6c1073db6cc0a6812263f
omar <omar@alx.com>
2023-04-20 20:03:11 -0700

    task 3

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -4,6 +4,12 @@
 #include <stdio.h>
 #include <stdarg.h>
 
+typedef struct token
+{
+	char *token;
+	void (*f)(char *, va_list);
+}token_t;
+
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
