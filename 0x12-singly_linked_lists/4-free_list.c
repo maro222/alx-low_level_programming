3659ca5a0cbbb9d2de0998c4302ff5dd29142699
omar <omar.a.farouk22@gmail.com>
2023-04-27 18:28:36 -0700

    task

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - list
+ * @head: head of list
+ *
+ * Return: nothing
+ */
+
+void free_list(list_t *head)
+{
+	list_t ptr;
+
+	while (head)
+	{
+		ptr = head;
+		head = head->next;
+		free(ptr);
+	}
+}
