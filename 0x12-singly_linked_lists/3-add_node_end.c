d82d8f7b908cf73bb9d08d888db02f8a988dafca
omar <omar.a.farouk22@gmail.com>
2023-04-27 18:14:40 -0700

    task

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * add_node_end - add
+ * @head: head
+ * @str: string
+ *
+ * Return: nothing
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *node;
+	struct list_s ptr;
+
+	node = malloc(sizeof(list_t));
+	if (!node)
+		return (NULL);
+
+	node->str = strdup(str);
+	node->len = strlen(str);
+	node->next = NULL;
+
+	if (*head == NULL)
+		*head = node;
+	else
+	{
+		ptr = *head;
+		while (ptr->next)
+			ptr = ptr->next;
+		ptr->next = node;
+	}
+	return (*head);
+}
