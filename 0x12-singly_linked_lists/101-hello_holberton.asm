7d6143325b4133f632a0710d54f762ce666bce94
omar <omar.a.farouk22@gmail.com>
2023-04-27 19:48:46 -0700

    task

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,19 @@
+org 100h
+
+.model small
+.data
+	myvar db "Hello, Holberton"
+
+.code
+main proc
+   mov ax,@data
+   mov ds,ax
+
+   mov dx,offset myvar
+   lea dx,myvar
+
+   mov ah,9
+   int 21h
+main endp
+
+ret
