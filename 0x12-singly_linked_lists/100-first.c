815f7529dc2e1fb87a91d4b71ceb50754efeb808
omar <omar.a.farouk22@gmail.com>
2023-04-27 18:49:16 -0700

    task

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include "lists.h"
+
+/**
+ * premain -  exe
+ * Return: void
+ */
+void __attribute__ ((constructor)) premain()
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
+
