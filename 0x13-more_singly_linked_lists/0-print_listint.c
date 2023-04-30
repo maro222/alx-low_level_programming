1ba05025d0df4e2fe95d64173733e9f3a6b679d2
omar <omar.a.farouk22@gmail.com>
2023-04-30 16:52:24 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+
+/**
+ *print_listint - print the list
+ *@h: header of list
+ *
+ *Return: no of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	listint *ptr = h;
+	int cnt = 0;
+
+	while (ptr)
+	{
+		ptr = ptr->next;
+		cnt++;
+	}
+	return (cnt);
+}
