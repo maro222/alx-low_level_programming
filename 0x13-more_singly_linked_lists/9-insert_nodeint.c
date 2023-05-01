f0b44c83f43ccc09b143dc9a6d4358abb0678246
omar <omar.a.farouk22@gmail.com>
2023-05-01 16:44:20 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,10 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (cnt == idx)
 		{
 			node->next = ptr;
-			temp->next = node;
+			if (cnt == 0)
+				*head = node;
+			else
+				temp->next = node;
 			return (node);
 		}
 		cnt++;
