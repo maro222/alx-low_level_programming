7dfbe87724a7a337bdc09c5ac8e9469455398d8e
omar <omar.a.farouk22@gmail.com>
2023-05-04 16:39:49 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,12 +1,29 @@
 #include "main.h"
 
-/***/
+/**
+ * print_binary - print
+ *@n: number
+ *
+ *Return: binary
+ */
 
 
 void print_binary(unsigned long int n)
 {
-	int temp;
+	int bits = sizeof(n) * 8, flagprint = 0;
 
-	while (n)
+	while (bits)
 	{
-		if (n 
+		if (n & 1l << --bits)
+		{
+			printf("1");
+			flagprint++;
+		}
+		else if (flagprint)
+		{
+			printf("0");
+		}
+	}
+		if (!flagprint)
+			printf("0");
+}
