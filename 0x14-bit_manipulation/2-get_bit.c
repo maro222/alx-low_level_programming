460bfb44747829eca897438a27a50deca25f5746
omar <omar.a.farouk22@gmail.com>
2023-05-04 17:41:50 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
-/***/
+/**
+ *get_bit - get
+ *@n: num
+ *@index: index
+ *Return: 1 or 0
+ */
 
 int get_bit(unsigned long int n, unsigned int index)
 {
@@ -8,29 +13,31 @@ int get_bit(unsigned long int n, unsigned int index)
 	int bits = sizeof(n) * 8, flagprint = 0;
 	unsigned int i = 0;
 
-	if (index >= bits)
+	if (index >= sizeof(n) * 8)
 		return (-1);
+	i = bits - 1;
+
 	while (bits)
 	{
 		if (n & 1l << --bits)
 		{
 			if (index == i)
 			{
-				printf("1");
-				return 0;
+				return (1);
 			}
-			i++;
-			flagprint++;
+			i--;
+			flagprint = 1;
 		}
 		else if (flagprint)
 		{
 			if (index == i)
 			{
-				printf("0");
-				return 0;
+				return (0);
 			}
-			i++;
+			i--;
 		}
+		else
+			i--;
 	}
-	printf("0");
+	return (0);
 }
