2dd028d89dd95c4d14002c0558218f93ca69339c
omar <omar.a.farouk22@gmail.com>
2023-05-04 18:14:30 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *set_bit -set
+ *@n: kk
+ @index: ind
+ *Return: iii
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= sizeof(n) * 8)
+		return (-1);
+
+	return (!!(*n |= 1l << index));
+}
+
