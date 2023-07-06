289cc86cb164f8fd6047d7248f312e9703d8a169
omar <omar.a.farouk22@gmail.com>
2023-07-06 13:20:52 -0700

    bit manipulation

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *binary_to_uint - from binary to decimal
+ *
+ *@b : binary number formatted string
+ *Return: decimal number
+ *
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i, j, idx, temp, dec = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
+	for (i = 0; b[i] != '\0'; i++)
+		;
+	i--;
+
+	for (j = i; j >= 0; j--)
+	{
+		idx = b[i - j] - '0';
+		if (idx == 0)
+		{
+			continue;
+		}
+		temp = j;
+		while (temp--)
+			idx *= 2;
+		dec += idx;
+	}
+	return (dec);
+}
