e989e0c69876154e94a44be3a70bb159ca1fa167
omar <omar.a.farouk22@gmail.com>
2023-07-07 08:29:32 -0700

    task 4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- *
  *clear_bit - make certain bit unset
  *
  *@n: certain number
  *@index: index of needed bit yo usnet
  *
- * return: the modified number
+ *Return: the modified number
  */
 
 int clear_bit(unsigned long int *n, unsigned int index)
