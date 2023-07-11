68839964f918e2bb94c304cf1b46623d6f16a9e1
omar <omar.a.farouk22@gmail.com>
2023-07-10 18:09:29 -0700

    Etask 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -29,7 +29,7 @@ int create_file(const char *filename, char *text_content)
 	{
 		letters++;
 	}
-	if (text_content != NULL)
+	if (text_content == NULL)
 		text_content = "";
 	wr = write(fd, text_content, letters);
 	if (wr < 0)
