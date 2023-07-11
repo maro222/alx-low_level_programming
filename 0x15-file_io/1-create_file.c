cf94a4a3145b564f65986a20dfe1cad0cb19c768
omar <omar.a.farouk22@gmail.com>
2023-07-10 17:02:04 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	cr = create(filename, S_IRUSR | S_IWUSR);
+	cr = creat(filename, S_IRUSR | S_IWUSR);
 	if (cr < 0)
 		return (-1);
 	fd = open(filename, O_RDWR | O_TRUNC);
