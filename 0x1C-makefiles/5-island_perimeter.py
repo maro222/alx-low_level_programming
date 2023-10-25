285aff5f6316276a2ba9d734eaab9e3297b09503
omar <omar.a.farouk22>
2023-10-25 16:30:40 -0700

    5-island_perimeter.py

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Module for technical interview """
+
+
+def islandPerimeter(self, grid: List[List[int]]) -> int:
+    """ function def island_perimeter(grid):
+    that returns the perimeter of the island described in grid
+    leetcode 463 island perimeter"""
+    sum = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] == 1:
+                sum = sum + 4
+                if i != 0 and grid[i - 1][j] == 1:
+                    sum = sum - 2
+                if j != 0 and grid[i][j - 1] == 1:
+                    sum = sum - 2
+    return (sum)
